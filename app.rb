4284a430b2c676906e64264650660333dbd4460a
Rohan317685 <dsairohan@gmail.com>
2026-03-29 00:56:02 +0000

    Fixed session error and added auth callback

--- app.rb
+++ app.rb
@@ -2,15 +2,27 @@ require 'sinatra'
 require 'omniauth-hack_club'
 require 'dotenv/load'
 
+set :bind, '0.0.0.0'
+set :port, 4567
+
+set :sessions, true
+set :session_secret, 'ioh1udg827tgi7yuagbcyuydnga8t8xgy1xc2ydtba76t9cv7ntg8ng2uy4efbc76nec8waiugbdvuagvw'
+
+use OmniAuth::Builder do 
+    provider :hack_club, ENV["HACKCLUB_CLIENT_ID"], ENV["HACKCLUB_CLIENT_SECRET"],
+    scope: "openid email name"
+end
+
 
 get '/' do
     send_file 'index.html'
 end 
 
-use OmniAuth::Builder do 
-    provider :hack_club, ENV["HACKCLUB_CLIENT_ID"], ENV["HACKCLUB_CLIENT_SECRET"]
-end
+get '/auth/hack_club/callback' do
+    auth = request.env['omniauth.auth']
+    "Hello #{auth['info']['name']}! Your logged in!"
+end 
 
-get 'auth/hack_club/callback' do
-    "Your logged in!"
+get '/auth/failure' do
+    "Login failed! Message: #{params[:message]}"
 end 
